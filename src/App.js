ff9084165c3121f7bcb747c84960e0acaf42e3de
AyaDesigner <aya.berdyeva@gmail.com>
2020-12-10 20:59:48 +0100

    removed unused comps

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import './App.css';
 import React from 'react';
 import Game from './components/Game';
 import Axios from 'axios';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-
 
 // import Timer from './components/Timer';
 
